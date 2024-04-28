0e0bf3511897ec8e31937fbf77cf1cecdbfc1295
Amit <achaudhary8118@gmail.com>
2024-04-28 16:04:38 +0530

    changes in first java program

--- Online_Java_Selenium_Training_20th_Jan_2020/src/javaSeleniumPackage/First_Java_Program.java
+++ Online_Java_Selenium_Training_20th_Jan_2020/src/javaSeleniumPackage/First_Java_Program.java
@@ -8,7 +8,7 @@ public class First_Java_Program {
 		System.out.println("Hi this is our first program");
 		System.out.println("Hello Selenium");
 		System.out.print("Amit");
-		System.out.print("a");
+		
 		
 	}    
 
